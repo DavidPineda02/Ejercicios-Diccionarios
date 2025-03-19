e8de27e7fcdae679d1bfe7991b94d089b2dad909
DavidPineda02 <davxpa.02@gmail.com>
2025-03-19 16:22:56 -0500

    Septimo Ejercicio Investigativo - Funcion items()

---
+++ Actividad 3.2/ejerc7.py
@@ -0,0 +1,13 @@
+#7. print(direccion_datos.items()) # cual es el uso de esta función
+
+datosPerso = {
+    "nombre": "Carlos",
+    "calle": "Avenida de la Libertad",
+    "número": 25,
+    "ciudad": "Madrid",
+    "código_postal": "28001",
+    "país": "España"
+}
+
+print("Los valores de la dirección son:")
+print(datosPerso.items())
