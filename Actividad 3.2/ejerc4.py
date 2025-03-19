b9174a6fa1c3fbf9cf858458af8a25301ac1dc47
DavidPineda02 <davxpa.02@gmail.com>
2025-03-19 14:56:16 -0500

    Cuarto Ejercicio Investigativo - Funcion len()

---
+++ Actividad 3.2/ejerc4.py
@@ -0,0 +1,14 @@
+# 4. Cuál es la función que permite devolver el número de elementos que tiene el diccionario
+
+libro = {
+    "titulo": "Cien Años de Soledad",
+    "autor": "Gabriel Gracia Marquez",
+    "año": 1967,
+    "genero": "Realismo Magico",
+    "paginas": 417
+}
+
+cantElemnt = len(libro)
+
+print(f"Diccionario: {libro}")
+print(f"Cantidad de Elementos del Diccionario: {cantElemnt}")
