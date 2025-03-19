e2762e165750e1e24cd049ae5b5d4039967f4082
DavidPineda02 <davxpa.02@gmail.com>
2025-03-19 16:17:27 -0500

    Sexto Ejercicio Investigativo - Funcion values()

---
+++ Actividad 3.2/ejerc6.py
@@ -0,0 +1,13 @@
+# 6. print(direccion_datos.values()) # cual es el uso de esta función incorporar a un ejercicio.  
+
+datosPerso = {
+    "nombre": "Carlos",
+    "calle": "Avenida de la Libertad",
+    "número": 25,
+    "ciudad": "Madrid",
+    "código_postal": "28001",
+    "país": "España"
+}
+
+print("Los valores de la dirección son:")
+print(datosPerso.values())
