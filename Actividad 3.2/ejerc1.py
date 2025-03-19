9700c1e0a00e370005e04e3e7e8f0a1849ea1bae
DavidPineda02 <davxpa.02@gmail.com>
2025-03-19 14:14:07 -0500

    Primer Ejercicio Investigativo - Funcion len()

---
+++ Actividad 3.2/ejerc1.py
@@ -0,0 +1,11 @@
+# 1. Identificar la función que devuelva el número de elementos que tiene un diccionario, dar ejemplos.
+
+myInfo = {
+    "nombre": "David",
+    "edad": 19,
+    "ciudad": "Bucaramanga"
+}
+
+numsElemts = len(myInfo)
+
+print(f"Cantidad de Elementos: {numsElemts}")
