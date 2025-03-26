eee2b58827055d50faa26fda1b3b8646c8200df3
DavidPineda02 <davxpa.02@gmail.com>
2025-03-26 14:20:05 -0500

    Documentando el Ejercicio 4

--- Actividad 3.2/ejerc4.py
+++ Actividad 3.2/ejerc4.py
@@ -1,14 +1,17 @@
 # 4. Cuál es la función que permite devolver el número de elementos que tiene el diccionario
 
+# Se crea un diccionario con información sobre un libro
 libro = {
-    "titulo": "Cien Años de Soledad",
-    "autor": "Gabriel Gracia Marquez",
-    "año": 1967,
-    "genero": "Realismo Magico",
-    "paginas": 417
+    "titulo": "Cien Años de Soledad",       # Clave "titulo" con el nombre del libro
+    "autor": "Gabriel García Márquez",      # Clave "autor" con el nombre del escritor
+    "año": 1967,                            # Clave "año" con el año de publicación
+    "genero": "Realismo Mágico",            # Clave "genero" con el género literario
+    "paginas": 417                          # Clave "paginas" con el número de páginas
 }
 
+# Se obtiene la cantidad de elementos en el diccionario
 cantElemnt = len(libro)
 
-print(f"Diccionario: {libro}")
+# Se imprimen los resultados
+print(f"Diccionario: {libro}")  
 print(f"Cantidad de Elementos del Diccionario: {cantElemnt}")
