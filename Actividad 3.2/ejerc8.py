d5ad9f5a139a1f5682a5226579410d9380df3fb6
DavidPineda02 <davxpa.02@gmail.com>
2025-03-26 14:22:06 -0500

    Documentando el Ejercicio 8

--- Actividad 3.2/ejerc8.py
+++ Actividad 3.2/ejerc8.py
@@ -1,20 +1,25 @@
 #8. Función que permite añadir los elementos de un diccionario a otro. 
 
+# Se crea el primer diccionario con información personal
 dicc1 = {
-    "nombre": "David",
-    "edad": 19,
-    "ciudad": "Piedecuesta"
+    "nombre": "David",          # Clave "nombre" con el valor "David"
+    "edad": 19,                 # Clave "edad" con el valor 19
+    "ciudad": "Piedecuesta"      # Clave "ciudad" con el valor "Piedecuesta"
 }
 
+# Se crea el segundo diccionario con información adicional
 dicc2 = {
-    "ocupacion": "Ingeniera",
-    "hobbies": ["Baloncesto", "Codificar"],
-    "ciudad": "Madrid"
+    "ocupacion": "Ingeniera",               # Clave "ocupacion" con el valor "Ingeniera"
+    "hobbies": ["Baloncesto", "Codificar"], # Clave "hobbies" con una lista de pasatiempos
+    "ciudad": "Madrid"                      # Clave "ciudad" con el valor "Madrid"
 }
 
+# Se imprimen los diccionarios antes de la actualización
 print(f"Diccionario 1 Antes de Actualizar: {dicc1}")
 print(f"Diccionario 2: {dicc2}")
 
+# Se actualiza dicc1 con los valores de dicc2
 dicc1.update(dicc2)
 
-print(f"\nDiccionario 1 Despues de Actualizar: {dicc1}")
+# Se imprime dicc1 después de la actualización
+print(f"\nDiccionario 1 Después de Actualizar: {dicc1}")
