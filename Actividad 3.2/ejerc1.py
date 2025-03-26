ea39c06aafd1ec6228114a5a777d8f076b97377b
DavidPineda02 <davxpa.02@gmail.com>
2025-03-26 14:17:26 -0500

    Documentando el Ejercicio 1

--- Actividad 3.2/ejerc1.py
+++ Actividad 3.2/ejerc1.py
@@ -1,11 +1,14 @@
 # 1. Identificar la función que devuelva el número de elementos que tiene un diccionario, dar ejemplos.
 
+# Definimos un diccionario con información personal
 myInfo = {
-    "nombre": "David",
-    "edad": 19,
-    "ciudad": "Bucaramanga"
+    "nombre": "David",  # Clave "nombre" con valor "David"
+    "edad": 19,         # Clave "edad" con valor 19
+    "ciudad": "Bucaramanga"  # Clave "ciudad" con valor "Bucaramanga"
 }
 
+# Calculamos la cantidad de elementos (pares clave-valor) en el diccionario
 numsElemts = len(myInfo)
 
+# Imprimimos la cantidad de elementos en el diccionario
 print(f"Cantidad de Elementos: {numsElemts}")
