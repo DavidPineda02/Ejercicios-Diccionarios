bbdb612ea911d8e240ce94b0109e62e4c3bb7e9d
DavidPineda02 <davxpa.02@gmail.com>
2025-03-26 14:19:24 -0500

    Documentando el Ejercicio 3

--- Actividad 3.2/ejerc3.py
+++ Actividad 3.2/ejerc3.py
@@ -1,13 +1,16 @@
 # 3. Cuál es la función que permite eliminar algún elemento del diccionario.
 
+# Se crea un diccionario con elementos favoritos
 cosasFav = {
-    "color": "Negro",
-    "comida": "Papas Locas",
-    "deporte": "Baloncesto"
+    "color": "Negro",        # Clave "color" con valor "Negro"
+    "comida": "Papas Locas", # Clave "comida" con valor "Papas Locas"
+    "deporte": "Baloncesto"  # Clave "deporte" con valor "Baloncesto"
 }
 
+# Se elimina el elemento con clave "comida" y se almacena su valor en 'valrElimn'
 valrElimn = cosasFav.pop("comida")
 
-print(f"Diccionario: {cosasFav}")
-print(f"Valor eliminado: {valrElimn}")
-print(f"Diccionario después de la eliminación: {cosasFav}")
+# Se imprimen los resultados
+print(f"Diccionario: {cosasFav}")  # Se muestra el diccionario después de la eliminación
+print(f"Valor eliminado: {valrElimn}")  # Se muestra el valor eliminado
+print(f"Diccionario después de la eliminación: {cosasFav}")  # Se verifica nuevamente el diccionario
